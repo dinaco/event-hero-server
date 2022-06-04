2b392cf2230d076b30d1f0830202ddb7003b6e45
Dino Marchiori <dinaco1@gmail.com>
2022-06-04 20:44:17 +0100

    added profileImg to payload

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -113,9 +113,9 @@ router.post("/login", (req, res, next) => {
         if (!isSamePassword) {
           return res.status(400).json({ errorMessage: "Wrong credentials." });
         }
-        const { _id, email, name, role } = user;
+        const { _id, email, name, role, profileImg } = user;
 
-        const payload = { _id, email, name, role };
+        const payload = { _id, email, name, role, profileImg };
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
           algorithm: "HS256",
           expiresIn: "14d",
